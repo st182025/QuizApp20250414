b9da76b66d7f827afc0ea9069e521066b65041bd
sbt18hurang <shibatatakumi.hurang@gmail.com>
2025-04-14 18:35:56 +0900

    問題作成フォーム

--- src/main/java/com/quizapp/repository/CategoryRepository.java
+++ src/main/java/com/quizapp/repository/CategoryRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import com.quizapp.entity.CategoryEntity;
 
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Integer> {
-
+	CategoryEntity findByName(String name);
 	// カテゴリ名（name）からカテゴリID（id）を取得するJPQLクエリ
 	// CategoryEntity を c という別名（エイリアス）で扱い、c.name で名前検索
     @Query("SELECT c.id FROM CategoryEntity c WHERE c.name = :name")
